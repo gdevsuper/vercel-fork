4d5fab07e3907f4f58e1c7aba4ccbfc66dd763fe
webmastercraft <iakov.agababian2020@gmail.com>
2022-02-03 16:35:17 +0300

    update token address as DGTL

--- src/config/constants/tokens.ts
+++ src/config/constants/tokens.ts
@@ -23,11 +23,11 @@ export const mainnetTokens = defineTokens({
   bnb: new Token(MAINNET, '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', 18, 'BNB', 'BNB', 'https://www.binance.com/'),
   cake: new Token(
     MAINNET,
-    '0x0E09FaBB73Bd3Ade0a17ECC321fD13a19e81cE82',
+    '0x210ab9b551e81b7ca2979b06bb64eb4e774b360b',
     18,
-    'CAKE',
-    'PancakeSwap Token',
-    'https://pancakeswap.finance/',
+    'DGTL',
+    'DGTL Token',
+    'https://dgtl-pancake.vercel.app/',
   ),
   tlos: new Token(MAINNET, '0xb6C53431608E626AC81a9776ac3e999c5556717c', 18, 'TLOS', 'Telos', 'https://www.telos.net/'),
   beta: new Token(
